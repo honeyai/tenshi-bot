f4fe0435f528c6e37bc05ca595f44f469d53eaa0
Jessica Sanchez <contact.jessicasanchez@gmail.com>
2020-11-05 15:31:02 -0500

    env didn't work, using json

--- src/bot.js
+++ src/bot.js
@@ -1,1 +1,8 @@
 //The entry point to the bot
+
+//importing env variable => loads up all the env variables
+//! if os has an env with the same name it will interpret variable with os' variable of the same name
+
+const botToken = require('../envDoesntWork.json').BOT_TOKEN;
+
+// console.log(botToken, "i would've shown up");
