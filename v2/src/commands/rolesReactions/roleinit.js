79f99d5a1f5aba2493fa3184c28f9387e5ad7bc0
Jessica Sanchez <contact.jessicasanchez@gmail.com>
2020-11-20 14:50:44 -0500

    getting responses from user for roles. Checking rolenames first

--- v2/src/commands/rolesReactions/roleinit.js
+++ v2/src/commands/rolesReactions/roleinit.js
@@ -1,9 +1,31 @@
 const { MessageCollector } = require("discord.js");
 
-let messageFilter = (message, original) => {
-  
-  message.author.id === original.author.id ? true : false;
-}
+let messageFilter = async (message, original) => {
+  let [emojiName, roleName] = original.content.toLowerCase().split(/,\s+/);
+  let { cache } = message.guild.roles;
+  //making sure that another message doesn't interfere
+  let isSameAuthor = message.author.id === original.author.id ? true : false;
+  if (!original.author.bot) {
+    //making sure that the message follows the right model
+    if (!emojiName && !roleName && !message.author.bot) {
+      message.channel.send(
+        "Please follow this structure 'emoji name, role name' for correct set up."
+      );
+      return false; //don't collect this message
+    } else {
+      if (cache.find((role) => role.name === roleName)) {
+        if (isSameAuthor) {
+          message.channel.send(
+            `You may enter the next role.`
+          );
+          return true;
+        }
+      } else {
+        message.channel.send(`The role, ${roleName}, doesn't exist.\nPlease create this role and try again.`)
+      }
+    }
+  }
+};
 
 module.exports = {
   run: async (client, message, args) => {
@@ -15,12 +37,14 @@ module.exports = {
       await msg.delete({ timeout: 5000 }).catch((error) => console.log(error));
     } else {
       try {
-        let fetched = await message.channel.messages.fetch(args);
+        let collector = new MessageCollector(
+          message.channel,
+          messageFilter.bind(null, message) //first null because function isn't being bound to an object, and message is any additional params
+        );
+        let fetched = await message.channel.messages.fetch(args[0]);
         if (fetched) {
-          message.channel.send("Provide emoji name and the role.")
-          let collector = new MessageCollector(
-            message.channel,
-            messageFilter.bind(null, message) //first null because function isn't being bound to an object, and message is any additional params
+          message.channel.send(
+            "Provide emoji name and the role. \nex. amongus, imposter \nSend each pair in a new message."
           );
           collector.on("collect", (msg) => {
             console.log(`${msg.content} was collected`);
