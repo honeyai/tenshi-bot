4fd4186f5dfe650a806d310a624cba50e2cdf49b
Angel Juarez <angeljuarez729@gmail.com>
2020-11-19 23:56:54 -0500

    starting new feat

---
+++ v2/src/commands/japanese/hiragana.js
@@ -0,0 +1,23 @@
+const alpha = require('../../characters');
+console.log(alpha);
+
+module.exports.run = async (client, message, args) => {
+  const japaneseChannel = message.channel === '778766867032440852';
+  if(!japaneseChannel) {
+    try {
+      await message.reply('All Japanese commands should be sent to #japanese!!')
+    } catch(e) {
+      console.error(e);
+    }
+  } else {
+    // commands here
+      // random - the hiragana
+      // random - the romanji
+    // random for both together
+    // random for either - you guess the other thing
+      // if they send hiragana - you guess the romanji
+      // if they send romanji - you send hiragna
+    // if you send the hiragana itself (with no request for random) - they send romanji
+    // if you send the romanji itself (with no request for random) - they send hiragana
+  }
+}
