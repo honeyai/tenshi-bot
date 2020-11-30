0f55029d9c550cd592f07c3c873a2550bf9972f7
Jessica Sanchez <contact.jessicasanchez@gmail.com>
2020-11-30 15:55:50 -0500

    Mapped roles with emoji

--- v2/src/commands/rolesReactions/roleinit.js
+++ v2/src/commands/rolesReactions/roleinit.js
@@ -1,62 +1,8 @@
 const { MessageCollector } = require("discord.js");
 const MessageModel = require("../../database/models/message.js");
 
-let messageFilter = async (message, original) => {
-  if (
-    original.content
-      .toLowerCase()
-      .split(/,\s+/)
-      .includes("exit", "done", "finished")
-  ) {
-    console.log(original.content)
-    console.log("I heard you");
-  } else {
-    console.log("I'm ignoring the condition")
-    let [emojiName, roleName] = original.content.toLowerCase().split(/,\s+/);
-    let roleFind = message.guild.roles.cache.find(
-      (role) => role.name === roleName
-    );
-    let emojiFind = message.guild.emojis.cache.find(
-      (emoji) => emoji.name === emojiName
-    );
-    //making sure that another message doesn't interfere
-    let isSameAuthor = message.author.id === original.author.id ? true : false;
-    if (!original.author.bot) {
-      //making sure that the message follows the right model
-      if (!emojiName && !roleName && !message.author.bot) {
-        message.channel.send(
-          "Please follow this structure 'emoji name, role name' for correct set up."
-        );
-        return false; //don't collect this message
-      } else {
-        if (roleFind && emojiFind) {
-          if (isSameAuthor) {
-            message.channel.send(
-              `You may enter the next role. Or send "exit", "done", or "finished" to complete initialization.`
-            );
-            return true;
-          }
-        } else {
-          if (!roleFind) {
-            message.channel
-              .send(
-                `The role, ${roleName}, doesn't exist.\nPlease create this role and try again.`
-              )
-              .then((msg) => msg.delete({ timeout: 3000 }))
-              .catch((error) => console.error);
-          } else {
-            message.channel
-              .send(
-                `The emoji, ${emojiName}, doesn't exist.\nPlease create this emoji or type in the correct emoji name and try again.`
-              )
-              .then((msg) => msg.delete({ timeout: 3000 }))
-              .catch((error) => console.error);
-          }
-        }
-      }
-    }
-  }
-};
+let messageFilter = async (message, original) =>
+  message.author.id === original.author.id && !original.author.bot;
 
 module.exports = {
   run: async (client, message, args) => {
@@ -79,18 +25,10 @@ module.exports = {
           );
           let emojiRoleMap = new Map();
           collector.on("collect", (msg) => {
-            //breaking out of the ask
-            if (
-              msg.content
-                .toLowerCase()
-                .split(/,\s+/)
-                .includes("exit", "done", "finished")
-            ) {
-              console.log("I've spotted the words but oh well")
-              collector.stop("Reactions initialized.");
+            if (msg.content.toLowerCase() === 'done') {
+              collector.stop('No longer waiting for roles.');
               return;
             }
-            console.log(`${msg.content} was collected`);
             //all the messages that passed the filter will have to be parsed again.
             let [emojiName, roleName] = msg.content.toLowerCase().split(/,\s+/);
             let roleFind = msg.guild.roles.cache.find(
@@ -99,19 +37,30 @@ module.exports = {
             let emojiFind = msg.guild.emojis.cache.find(
               (emoji) => emoji.name === emojiName
             );
-            if (emojiFind && roleFind) {
+            if (!emojiName || !roleName) message.channel.send(`Please send your message as a pair separated with a coma. \nex. amongus, imposter.`);
+            else if (emojiFind && roleFind) {
+              message.channel.send(
+                `You may enter the next role. Or send "$done" to complete initialization.`
+              );
               fetched
                 .react(emojiFind)
                 .then((emoji) => console.log("reacted with " + emojiFind + "."))
                 .catch((error) => console.error(error));
-              emojiRoleMap.set(emojiFind, roleFind);
-              // let stored = new MessageModel({
-              //   messageId: fetched,
-              //   emojiRole: { "`${emojiFind}`": roleFind },
-              // })
-              //   .save()
-              //   .then((saved) => console.log(saved))
-              //   .catch((error) => console.error(error));
+              emojiRoleMap.set(emojiFind.id, roleFind.id);
+            } else if (!roleFind) {
+              message.channel
+                .send(
+                  `The role, ${roleName}, doesn't exist.\nPlease create this role and try again.`
+                )
+                .then((msg) => msg.delete({ timeout: 3000 }))
+                .catch((error) => console.error(error));
+            } else if (!emojiFind) {
+              message.channel
+              .send(
+                `The emoji, ${emojiName}, doesn't exist.\nPlease create this emoji or type in the correct emoji name and try again.`
+              )
+              .then((msg) => msg.delete({ timeout: 3000 }))
+              .catch((error) => console.error(error));
             }
           });
           collector.on("end", (collected, reason) => {
